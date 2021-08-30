3d1d2b4d1c2a3d0687daa7b77ceb05027b31e5b5
Freya-LR <layla.dnlii@gmail.com>
2021-08-29 23:35:51 -0400

    Create 70.Climbing Stairs.py

---
+++ 70.Climbing Stairs.py
@@ -0,0 +1,15 @@
+# Dynamic Programming
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def climbStairs(self, n):
+        """
+        type n: int
+        rtype: int
+        """
+        prev, current = 0, 1
+        for i in range(n):
+            prev, current = current, prev + current,
+
+        return current
