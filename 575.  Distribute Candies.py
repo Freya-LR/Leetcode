879080b0ef8612057c58ef3f72e39dc543a47393
Freya-LR <layla.dnlii@gmail.com>
2021-08-30 00:57:03 -0400

    Update 575.  Distribute Candies.py

--- 575.  Distribute Candies.py
+++ 575.  Distribute Candies.py
@@ -4,7 +4,7 @@
 # Alice can only eat n/2 candies, let her eat the larget type of candies.
 # if type is larger than n/2, she can just eat n/2 candies
 # if type is less than n/2, the types she can eat is number of types
-# so get the minimum number of two type of situation, len(candyType) >> 1 get the n/2 candies, len(set(candyType)) get the largest types
+# so get the minimum number of two type of situation, len(candyType) //2 get the n/2 candies, len(set(candyType)) get the largest types
 
 class Solution:
     def distributeCandies(self, candyType):
