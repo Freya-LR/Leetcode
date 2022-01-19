3599e607b578da7625bb55b63e3d424d55826792
Freya-LR <layla.dnlii@gmail.com>
2022-01-19 02:23:46 -0500

    Update and rename 182.Duplicate Emails.ipynb to 182.Duplicate Emails.sql

---
+++ 182.Duplicate Emails.sql
@@ -0,0 +1,22 @@
+Write an SQL query to report all the duplicate emails.
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+# Write your MySQL query statement below
+select email from Person
+group by email
+having count(*)>1
